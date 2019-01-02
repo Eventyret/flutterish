3c65a04ef16ba5594d375ce7b4ad30b057ca28c9
Simen Daehlin <simen.dehlin@gmail.com>
2019-01-02 22:00:54 +0000

    Added a container with margins

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("EasyList"),
-        ),
-        body: Card(
-          child: Column(
-            children: <Widget>[
-              Image.asset("assets/food.jpg"),
-              Text("Food Paradise")
-            ],
+          appBar: AppBar(
+            title: Text("EasyList"),
           ),
-        ),
-      ),
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('Add Product'),
+                ),
+              ),
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset("assets/food.jpg"),
+                    Text("Food Paradise")
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
