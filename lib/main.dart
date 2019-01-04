6311852f15096b04ad752a645615113e217c6450
Simen Daehlin <simen.dehlin@gmail.com>
2019-01-04 16:01:35 +0000

    Restructured Main and productmanager

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,17 @@
 import 'package:flutter/material.dart';
+import './product_manager.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> _products = ['Food Tester'];
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text("EasyList"),
-          ),
-          body: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: RaisedButton(
-                  onPressed: () {
-                    setState(() {
-                      _products.add('Advanced Food Tester');
-                    });
-                  },
-                  child: Text('Add Product'),
-                ),
-              ),
-              Column(
-                children: _products
-                    .map((element) => Card(
-                          child: Column(
-                            children: <Widget>[
-                              Image.asset("assets/food.jpg"),
-                              Text(element)
-                            ],
-                          ),
-                        ))
-                    .toList(),
-              )
-            ],
-          )),
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('EasyList'),
+      ),
+      body: ProductManager(),
+    ));
   }
 }
