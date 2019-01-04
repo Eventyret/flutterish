fb9aa308dfb5af78297fb80b4bb0e376c67bd47a
Simen Daehlin <simen.dehlin@gmail.com>
2019-01-04 16:36:50 +0000

    Passing data with supoer initState

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
         title: Text('EasyList'),
       ),
-      body: ProductManager(),
+      body: ProductManager('Food Tester'),
     ));
   }
 }
--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import './products.dart';
 
 class ProductManager extends StatefulWidget {
+  final String startingProduct;
+
+  ProductManager(this.startingProduct);
   @override
   State<StatefulWidget> createState() {
     return _ProductManagerState();
@@ -10,7 +13,13 @@ class ProductManager extends StatefulWidget {
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = ['Food Tester'];
+  List<String> _products = [];
+  
+  @override
+    void initState() {
+      super.initState();
+      _products.add(widget.startingProduct)
+    }
   @override
   Widget build(BuildContext context) {
     return Column(children: [
