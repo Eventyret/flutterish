87e29783d2e48e8b0c1960d8ea50d42416e21a26
Simen Daehlin <simen.dehlin@gmail.com>
2019-01-05 21:33:30 +0000

    Improved list made it scrollable

--- lib/products.dart
+++ lib/products.dart
@@ -6,7 +6,7 @@ class Products extends StatelessWidget {
   Products([this.products = const []]);
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: products
           .map((element) => Card(
                 child: Column(
