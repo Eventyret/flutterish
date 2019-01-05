baf9a2d4d2cd61267d358ba5a8f6f68a83dd6cdd
Simen Daehlin <simen.dehlin@gmail.com>
2019-01-05 21:43:58 +0000

    Remade list for better performance with builder

--- lib/products.dart
+++ lib/products.dart
@@ -4,19 +4,23 @@ class Products extends StatelessWidget {
   final List<String> products;
 
   Products([this.products = const []]);
+
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text(products[index])
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: products
-          .map((element) => Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset("assets/food.jpg"),
-                    Text(element)
-                  ],
-                ),
-              ))
-          .toList(),
+    return ListView.builder(
+      itemBuilder: _buildProductItem,
+      itemCount: products.length,
     );
   }
 }
